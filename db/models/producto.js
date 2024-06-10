7ab64029ef9305fa766dccd0604b24e6d0b22b09
Daniel <90976801+Daluca21@users.noreply.github.com>
2024-06-09 23:04:03 -0500

    Creado Models de Modelo y Producto

---
+++ db/models/producto.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes, Model) => {
+    class Modelo extends Model {}
+    
+    Modelo.init(
+      {
+        id: {
+          type: DataTypes.INTEGER, 
+          autoIncrement: true,
+          primaryKey: true
+        },
+        tamanio: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        cantidadDisponible: {
+          type: DataTypes.INTEGER,
+          allowNull: false
+        },
+        precio: {
+          type: DataTypes.DOUBLE,
+          allowNull: false
+        },
+        enabled: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+      },
+      {
+        sequelize, 
+        TableName: 'Modelo', 
+        createdAt: false,
+        updatedAt: false
+      },
+    );
+  };
