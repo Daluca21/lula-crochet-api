b61e8fc0f4c9c8b5353f7b61cbf9d22cb63e19a9
Daniel <90976801+Daluca21@users.noreply.github.com>
2024-06-20 22:54:39 -0500

    Creación transaccion con carrito

--- controllers/FacturaController.js
+++ controllers/FacturaController.js
@@ -2,22 +2,35 @@ const { client } = require("../config/mercadopago");
 const { Preference } = require("mercadopago");
 const { URL_FRONT, URL_BACK } = require("../config/index");
 const FacturaService = require("../services/FacturaService");
+const ProductoService = require("../services/ProductoService");
+const CarritoService = require("../services/CarritoService");
+const ModeloService = require("../services/ModeloService");
 const service = new FacturaService();
+const productoService = new ProductoService();
+const carritoService = new CarritoService();
+const modeloService = new ModeloService();
 
 const create = async (req, res) => {
     try {
         const preference = new Preference(client);
         const body = req.body;
-        
+        const correo = body.correo;
+        const carrito = await productoService.getCarrito(correo);
+        console.log(carrito);
+        const items = await Promise.all(carrito.map(async producto => {
+            const modelo = await modeloService.findOne(producto.id_modelo);
+            const cantidad = await carritoService.getAmountByUser(correo, producto.id);
+            const descuento = await productoService.getDescuento(producto);
+            return {
+                title: `${modelo.nombre} ${producto.tamanio}`,
+                quantity: cantidad,
+                unit_price: producto.precio * (1 - descuento / 100)
+            };
+        }));
+
         const response = await preference.create({
             body: {
-                items: [
-                    {
-                        title: 'Mi producto',
-                        quantity: 1,
-                        unit_price: 20000
-                    }
-                ],
+                items: items,
                 back_urls: {
                     "success": `${URL_FRONT}/api`,
                     "failure": `${URL_FRONT}/api`,
@@ -26,6 +39,7 @@ const create = async (req, res) => {
                 notification_url: `${URL_BACK}/pagos/webhook`
             }
         });
+
         res.json(response);
     } catch (error) {
         res.status(500).send({ success: false, message: error.message });
@@ -33,10 +47,10 @@ const create = async (req, res) => {
 };
 
 const recieveWebhook = async (req, res) => {
-    try{
-        console.log(req.query["data.id"]); 
+    try {
+        console.log(req.query);
         res.json({ success: true });
-    }catch(error){
+    } catch (error) {
         console.log(error);
         res.status(500).send({ success: false, message: error.message });
     }
